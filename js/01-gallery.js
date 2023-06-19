2221b19f7b33ddffcda2cab5dc52d438f96d4f51
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-06-19 17:35:02 +0200

    e

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const images = galleryItems
+  .map(
+    (image) =>
+      `<div class = "gallery__item"><a class = "gallery__link" href = "${image.original}"><img class = "gallery__image" data-source = "${image.original}" src = "${image.preview}"/></a></div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", images);
+console.log(gallery);
