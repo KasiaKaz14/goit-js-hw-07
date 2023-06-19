3f24c25dd92d0c5dce1eacbe6ebe04268f6f584d
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-06-19 18:19:27 +0200

    e

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,5 +8,28 @@ const images = galleryItems
       `<div class = "gallery__item"><a class = "gallery__link" href = "${image.original}"><img class = "gallery__image" data-source = "${image.original}" src = "${image.preview}"/></a></div>`
   )
   .join("");
+
 gallery.insertAdjacentHTML("beforeend", images);
 console.log(gallery);
+
+gallery.addEventListener("click", (e) => {
+  event.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const escapeKey = (e) => {
+    if (e.key === "Escape") instance.close();
+  };
+  const instance = basicLightbox.create(
+    `<img src = "${e.target.dataset.source}"/>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", escapeKey);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", escapeKey);
+      },
+    }
+  );
+
+  instance.show();
+});
